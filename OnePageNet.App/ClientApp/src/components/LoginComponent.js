6a794daf5cab3ae5f0f810c6bfe88f6b10bb191a
calculated <maga.ot.oz@protonmail.com>
2022-02-01 22:37:06 +0200

    Add some more

---
+++ OnePageNet.App/ClientApp/src/components/LoginComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class LoginComponent extends Component {
+  static displayName = LoginComponent.name;
+
+  render () {
+    return (
+        // TODO Add HTML BS
+        <div></div>
+    );
+  }
+}
