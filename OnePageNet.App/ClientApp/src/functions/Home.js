710565302c9179cb892811575a259c670079212b
Martin Svilenski <m.svilenski@gmail.com>
2022-04-09 00:39:38 +0300

    Done

--- OnePageNet.App/ClientApp/src/functions/Home.js
+++ OnePageNet.App/ClientApp/src/functions/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-export function Home() {
+import React, { useState } from 'react';
+import { Login } from './Login.js';
+import { Register } from './Register.js';
+import './styles/HomePage.css';
+export function Home({setToken, onLayoutChange}) {
+  const [homeState, setHomeState] = useState('login');
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        <Link to="/register">Register</Link><br/>
-        <Link to="/login">Login</Link> 
-      </p>
+    <div className='home'>
+      <div className='login-register-container'>
+        {
+          (() => {
+            if (homeState === 'login') {
+              return <Login setToken={setToken} onLayoutChange={onLayoutChange}/>
+            } else {
+              return <Register setToken={setToken}/>
+            }
+          })()
+        }
+        <div className='buttons'>
+          <button className='button' onClick={() => setHomeState('register')}>Register</button>
+          <button className='button' onClick={() => setHomeState('login')}>Login</button>
+        </div>
+      </div>
     </div>
   );
 };
